05a1a7614dc049d8b34292fddb73aa4e60a17e57
CodeSahil21 <sahil.s39026@email.com>
2026-01-10 17:39:33 +0530

    Auth-service was updated for docker

--- services/auth-service/prisma.config.ts
+++ services/auth-service/prisma.config.ts
@@ -1,7 +1,13 @@
-export default {
-  datasources: {
-    db: {
-      url: "env(DATABASE_URL)"
-    }
-  }
-}
+import 'dotenv/config';
+import { defineConfig } from '@prisma/config';
+
+const url = process.env.DATABASE_URL;
+
+
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
+  datasource: {
+    url,
+  },
+});
+
